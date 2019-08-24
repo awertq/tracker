8250e1cf89509178521d75d03f2d9a758db38cc1
awertq <awert2684@gmail.com>
2019-08-24 11:41:15 +0530

    csv file generator

--- csv_generator.py
+++ csv_generator.py
@@ -6,10 +6,18 @@ name = ["rocky",'bunty','raka', 'baba','parda']
 address = ['luchow','dedhi','gulabganj','waterway','amrika']
 
 data_count = int(input('data generator limit :  '))
+delimiter  = input('Enter delimiter ("," is default):  ')
+if delimiter == None or delimiter == ",":
+	delimiter = ","
 
-for i in range(data_count):
-	n = rd.choice(name)
-	a = rd.choice(address)
-	num = rd.randint(0,1000)
-	e = f'{n}{num}@email.com'
-	print('{}, {}, {}'.format(n,a,e))
+filename = input("enter filename  :")
+filename += ".csv"
+
+with open(filename,"w") as file:
+	for i in range(data_count):
+		n = rd.choice(name)
+		a = rd.choice(address)
+		num = rd.randint(0,1000)
+		e = f'{n}{num}@email.com'
+		dumdum = '{}{}{}{}{}'.format(n,delimiter,a,delimiter,e)
+		file.writelines(dumdum + '\n')
